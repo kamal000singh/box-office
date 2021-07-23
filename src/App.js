11fbc56d0b54786e2da93f0b6e50596f0af24135
kamal-singh <kamal000rawat@gmail.com>
2021-07-23 23:57:52 +0530

    added layout and create title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Nav from "./components/Nav.js";
 import Home from "./pages/Home.js";
 import Starred from "./pages/Starred.js";
 function App() {
   return (
     <div>
-      <Nav />
       <Switch>
         <Route exact path="/">
           <Home />

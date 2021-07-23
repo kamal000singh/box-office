e9f836177ac94a913309f11a67d57e2846aeb7dc
kamal-singh <kamal000rawat@gmail.com>
2021-07-23 23:34:21 +0530

    add nav bar and route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav.js";
+import Home from "./pages/Home.js";
+import Starred from "./pages/Starred.js";
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <h1>This is Page 1</h1>
-      </Route>
-      <Route exact path="/about">
-        <h1>This is About Page</h1>
-      </Route>
-      <Route>ERROR:404 PAGE</Route>
-    </Switch>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>ERROR:404 PAGE</Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Starred = () => {
+  return <div>This is a Starred page.</div>;
+};
+
+export default Starred;

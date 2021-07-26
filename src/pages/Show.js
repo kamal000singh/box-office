4b7954dba372312e298fb506c355622884cc5ba5
kamal-singh <kamal000rawat@gmail.com>
2021-07-26 13:48:23 +0530

    added show page features

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -3,15 +3,37 @@ import { useParams } from "react-router";
 import { getAPI } from "../misc/Config";
 const Show = () => {
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { id } = useParams();
   useEffect(() => {
-    return getAPI(`shows/${id}?embed[]=seasons&embed[]=cast`).then(
-      (results) => {
-        setShow(results);
-      }
-    );
+    let isMounted = true;
+    getAPI(`shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then((results) => {
+        setTimeout(() => {
+          if (isMounted) {
+            setShow(results);
+            setIsLoading(false);
+          }
+        }, 3000);
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
   console.log(show);
+  if (isLoading) {
+    return <div>Page is Loading</div>;
+  }
+  if (error) {
+    return <div>Error Occured:{error}</div>;
+  }
   return <div>hello</div>;
 };
 

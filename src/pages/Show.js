7388bce2faf53ec540d75136f70d6bcf4b948aab
kamal-singh <kamal000rawat@gmail.com>
2021-07-27 00:47:26 +0530

    Display show data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
 import React, { useReducer, useEffect } from "react";
 import { useParams } from "react-router";
+import { Cast } from "../components/shows/Cast";
+import { Details } from "../components/shows/Details";
+import { Seasons } from "../components/shows/Seasons";
+import { ShowMainData } from "../components/shows/ShowMainData";
+import { ShowPageWrapper } from "../components/styled";
 import { getAPI } from "../misc/Config";
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -48,7 +53,33 @@ const Show = () => {
   if (error) {
     return <div>Error Occured:{error}</div>;
   }
-  return <div>hello</div>;
+  return (
+    <ShowPageWrapper>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </ShowPageWrapper>
+  );
 };
 
 export default Show;
